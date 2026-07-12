f2fb6c439f467618de9b7a280137705e3d4943a3
neros <noslinedsotnas72@gmail.com>
2026-07-12 16:44:03 -0300

    Feat: Criação de empresa

---
+++ src/main/java/br/com/nerosct/front_gestao_vagas/modules/company/controller/CompanyController.java
@@ -0,0 +1,17 @@
+package br.com.nerosct.front_gestao_vagas.modules.company.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/company")
+public class CompanyController {
+
+
+    @GetMapping("/create")
+    public String create(){
+        return "company/create";
+    }
+
+}
